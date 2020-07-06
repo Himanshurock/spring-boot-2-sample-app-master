47418d3281b15967c2b18008e1279c9a08eada78
Himanshurock <himanshurock85@gmail.com>
2020-07-06 20:48:11 +0530

    Update MongTestIT.java

--- src/test/java/sample/actuator/MongTestIT.java
+++ src/test/java/sample/actuator/MongTestIT.java
@@ -7,13 +7,18 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.support.PersistenceExceptionTranslator;
+import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.mongodb.DB;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 
 
 class Student {
@@ -36,13 +41,10 @@ class Student {
 
 public class MongTestIT {
 	
-	//@Autowired
-	//MongoTemplate mongoTemplate;
+	@Autowired
+	//MongoTemplate mongoTemplate = new MongoTemplate(null);
 	
-	String LOCALHOST = System.getenv("HOSTNAME");
-	int MONGO_TEST_PORT = Integer.parseInt(System.getenv("MONGO_PORT"));
-	
-	MongoClient mongoClient = new MongoClient();
+
 	
 	
 	@Test
@@ -55,20 +57,53 @@ public class MongTestIT {
 		System.out.println("=============MONGO_PORT Env============="+System.getenv("MONGO_PORT"));
 		System.out.println("=============Hostname Env============="+System.getenv("HOSTNAME"));
 		
-		Mongo mongo =  new Mongo(LOCALHOST, MONGO_TEST_PORT);
+		//Mongo mongo =  new Mongo(LOCALHOST, MONGO_TEST_PORT);
+		
+		String LOCALHOST = System.getenv("HOSTNAME");
+		int MONGO_TEST_PORT = Integer.parseInt(System.getenv("MONGO_PORT"));
+		
+		//MongoClient mongoClient = new MongoClient();
+		
+		MongoDbFactory db= new MongoDbFactory() {
+			
+			@Override
+			public DB getLegacyDb() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public PersistenceExceptionTranslator getExceptionTranslator() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public MongoDatabase getDb(String dbName) throws DataAccessException {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public MongoDatabase getDb() throws DataAccessException {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
+		MongoTemplate mongoTemplate = new MongoTemplate(db);
 
 		Student stud = new Student();
 		stud.setFirstName("Test");
 		stud.setLastName("Mongo");
-	//	mongoTemplate.save(stud);
+		mongoTemplate.save(stud);
 		//mongoClient.setReadPreference(readPreference);
 	//	System.out.println("=============MongTestIT==========="+mongoClient+mongoClient.getConnectPoint()+mongoClient.getCredentialsList()+mongoClient.getAddress()+mongoClient.getAddress()+mongoClient.getDatabaseNames()+mongoClient.toString());
 
 		
 		Query query = Query.query(Criteria.where(stud.getFirstName()).is("Test"));
-		//List<Student> lst =  mongoTemplate.find(query, Student.class);
+		List<Student> lst =  mongoTemplate.find(query, Student.class);
 		
-		//System.out.println("===================="+lst.size());
+		System.out.println("=========size==========="+lst.size());
 		//Assert.assertEquals(1,lst.size());
 	}
 }

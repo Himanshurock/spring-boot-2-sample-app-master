ad8300a565f337b350af32d2cdbb4a57067f08c3
Himanshurock <himanshurock85@gmail.com>
2020-07-06 16:30:50 +0530

    Update MongTestIT.java

--- src/test/java/sample/actuator/MongTestIT.java
+++ src/test/java/sample/actuator/MongTestIT.java
@@ -1,34 +1,49 @@
-/*
- * package sample.actuator;
- * 
- * import java.util.List;
- * 
- * import org.junit.Test; import
- * org.springframework.beans.factory.annotation.Autowired; import
- * org.springframework.data.mongodb.core.MongoTemplate; import
- * org.springframework.data.mongodb.core.query.Criteria; import
- * org.springframework.data.mongodb.core.query.Query;
- * 
- * 
- * class Student { String firstName; String lastName; public String
- * getFirstName() { return firstName; } public void setFirstName(String
- * firstName) { this.firstName = firstName; } public String getLastName() {
- * return lastName; } public void setLastName(String lastName) { this.lastName =
- * lastName; }
- * 
- * } public class MongTestIT {
- * 
- * @Autowired MongoTemplate mongoTemplate;
- * 
- * 
- * @Test public void mongoTest() {
- * System.out.println("=============MongTestIT==========="); Student stud = new
- * Student(); stud.setFirstName("Test"); stud.setLastName("Mongo");
- * mongoTemplate.save(stud);
- * 
- * Query query = Query.query(Criteria.where(stud.getFirstName()).is("Test"));
- * List<Student> lst = mongoTemplate.find(query, Student.class);
- * 
- * System.out.println("===================="+lst.size());
- * //Assert.assertEquals(1,lst.size()); } }
- */
+package sample.actuator;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+
+class Student {
+	String firstName;
+	String lastName;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
+public class MongTestIT {
+	
+	@Autowired
+	MongoTemplate mongoTemplate;
+	
+	
+	@Test
+	public void mongoTest() {
+		System.out.println("=============MongTestIT===========");
+		Student stud = new Student();
+		stud.setFirstName("Test");
+		stud.setLastName("Mongo");
+		mongoTemplate.save(stud);
+		
+		Query query = Query.query(Criteria.where(stud.getFirstName()).is("Test"));
+		List<Student> lst =  mongoTemplate.find(query, Student.class);
+		
+		System.out.println("===================="+lst.size());
+		//Assert.assertEquals(1,lst.size());
+	}
+}

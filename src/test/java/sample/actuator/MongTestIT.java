0821e01cc4ed5dca8e596d99d5035d135813b699
Himanshurock <himanshurock85@gmail.com>
2020-07-06 18:06:42 +0530

    Update MongTestIT.java

--- src/test/java/sample/actuator/MongTestIT.java
+++ src/test/java/sample/actuator/MongTestIT.java
@@ -3,10 +3,14 @@ package sample.actuator;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.mongodb.MongoClient;
 
@@ -28,23 +32,29 @@ class Student {
 	}
 	
 }
+
 public class MongTestIT {
 	
 	@Autowired
 	MongoTemplate mongoTemplate;
 	
-	@Autowired
-	MongoClient mongoClient;
+	//MongoClient mongoClient = new MongoClient();
 	
 	
 	@Test
 	public void mongoTest() {
 		System.out.println("=============MongTestIT===========");
+		System.out.println("=============server.port============="+System.getProperty("server.port"));
+		System.out.println("=============server.port============="+System.getProperty("server.host"));
+		System.out.println("=============Allt============="+System.getProperties());
+
+
 		Student stud = new Student();
 		stud.setFirstName("Test");
 		stud.setLastName("Mongo");
-		//mongoTemplate.save(stud);
-		System.out.println("=============MongTestIT==========="+mongoClient+mongoClient.getConnectPoint()+mongoClient.getCredentialsList()+mongoClient.getAddress()+mongoClient.getAddress()+mongoClient.getDatabaseNames()+mongoClient.toString());
+	//	mongoTemplate.save(stud);
+		//mongoClient.setReadPreference(readPreference);
+	//	System.out.println("=============MongTestIT==========="+mongoClient+mongoClient.getConnectPoint()+mongoClient.getCredentialsList()+mongoClient.getAddress()+mongoClient.getAddress()+mongoClient.getDatabaseNames()+mongoClient.toString());
 
 		
 		Query query = Query.query(Criteria.where(stud.getFirstName()).is("Test"));
